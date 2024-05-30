3e18f6d5c23c0ff30c7ee501c339ea22887f09c5
cavityQED <mikeped@udel.edu>
2024-05-30 19:24:33 -0400

    FPGA data register
    Writes data always when wr_valid
    Only loads read byte on read input

--- fpga/pinc-fpga.srcs/sources_1/new/data_register.v
+++ fpga/pinc-fpga.srcs/sources_1/new/data_register.v
@@ -2,7 +2,7 @@ module data_register
 (
 	input 				clk,	// clock
 	input 				rst,	// reset
-	input				rw,		// read/write
+	input				read,
 
 	input		[7:0]	addr,
 
@@ -17,11 +17,11 @@ module data_register
 
 	always @(posedge clk) begin
 		rd_valid		<= 1'b0;
-		if(rw) begin					// read
+		if(read) begin					// read
 			rd_byte		<= data[addr];
 			rd_valid	<= 1'b1;
 		end
-		else if(~rw & wr_valid) begin	// write
+		else if(wr_valid) begin	// write
 			data[addr]	<= wr_byte;
 		end
 	end
