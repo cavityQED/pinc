125fdcc256cd200a0b22782fcc51bb845b60d52f
cavityQED <mikeped@udel.edu>
2024-06-07 16:44:23 -0400

    FPGA SPI
    Uses new edges module to get clock edges
    Assumes clock in has been 'sync'd' and 'debounced'
    Adds localparam for the max edge number based on MSB

--- fpga/pinc-fpga.srcs/sources_1/new/spi.v
+++ fpga/pinc-fpga.srcs/sources_1/new/spi.v
@@ -16,51 +16,39 @@ module spi
     
     input               tx_valid,
     output reg          tx_ready, 
-    output reg          rx_valid,
-
-    output              clk_out
+    output reg          rx_valid
 );
+
+    localparam  MAX_EDGE = 2*(MSB+1);
 
     reg [7:0] r_tx_byte;
     reg [2:0] r_tx_bit;
     reg [2:0] r_rx_bit;
     reg [4:0] r_edges;
 
-    wire   w_clk;
-    wire   w_clk_rise;
-    wire   w_clk_fall;
-    wire   w_clk_edge = w_clk_rise | w_clk_fall;
-    assign clk_out = w_clk;
+    wire w_clk_rise;
+    wire w_clk_fall;
+    edges clk_edges (clk, rst, spi_clk, w_clk_rise, w_clk_fall);
 
-    reg    spi_clk_rst;
-    reg    spi_clk_en;
-
-    debounce #(.TICK(5)) spi_clk_db
-    (
-        clk,
-        rst,
-        spi_clk_en,
-        spi_clk,
-        1'b0,
-
-        w_clk_rise,
-        w_clk_fall,
-        w_clk
-    );
+    initial begin
+        r_tx_byte   <= 8'h00;
+        r_tx_bit    <= MSB;
+        r_rx_bit    <= MSB;
+        r_edges     <= MAX_EDGE;
+        spi_miso    <= 1'b0;
+    end
 
     // clock
     always @(posedge clk) begin
         if(spi_cs) begin
-            r_edges     <= 2*(MSB+1);
-            spi_clk_en  <= 1'b0;
+            r_edges     <= MAX_EDGE;
         end
         else begin
-            spi_clk_en  <= 1'b1;
             if(tx_valid) begin
-                r_edges <= 2*(MSB+1);
+                r_edges <= MAX_EDGE;
             end
             else if(r_edges > 0) begin
-                if(w_clk_edge) begin
+                if(w_clk_rise | w_clk_fall) begin
                     r_edges <= r_edges - 1'b1;
                 end
             end
@@ -70,28 +58,28 @@ module spi
     // miso
     always @(posedge clk) begin
         tx_ready  <= 1'b0;
-        if(tx_valid) begin
-            r_tx_byte   <= tx_byte;
-            r_tx_bit    <= MSB;
-        end
         if(spi_cs) begin
             r_tx_bit    <= MSB;
-            r_tx_byte   <= 0;
-            spi_miso  <= 1'b0;
+            r_tx_byte   <= 8'h00;
+            spi_miso    <= 1'b0;
         end
         else begin
-            if(r_edges == 16) begin
-                spi_miso  <= r_tx_byte[MSB];
-                r_tx_bit    <= MSB - 1'b1;
+            if(tx_valid) begin
+                r_tx_byte   <= tx_byte;
+                r_tx_bit    <= MSB;
             end
             else if(w_clk_fall) begin
-                spi_miso  <= r_tx_byte[r_tx_bit];
+                spi_miso    <= r_tx_byte[r_tx_bit];
                 if(r_tx_bit > 0) begin
                     r_tx_bit    <= r_tx_bit - 1'b1;
                 end
-                if(r_edges == 1) begin
-                    tx_ready  <= 1'b1;
+                else if(r_edges == 1) begin
+                    tx_ready    <= 1'b1;
                 end
+            end
+            else if(r_edges == MAX_EDGE) begin
+                spi_miso    <= r_tx_byte[MSB];
+                r_tx_bit    <= MSB - 1'b1;
             end
         end
     end
