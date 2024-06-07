d0e11bc8786511832ad99b32841437e5295ab5f0
cavityQED <mikeped@udel.edu>
2024-06-07 16:47:36 -0400

    FPGA Main
    Reworks main fpga file to use new signal modules
    Removes wheel function for now
    Adds 8 out signals for logic analyzer purposes
    Currently 4 of 8 out signals are used to monitor the (debounced) x axis status

--- fpga/pinc-fpga.srcs/sources_1/new/main.v
+++ fpga/pinc-fpga.srcs/sources_1/new/main.v
@@ -9,183 +9,150 @@ module top
     input           spi_cs,
     input           spi_mosi,
     output          spi_miso,
-    
-    output          wheel_high,
 
-    input [1:0]     wheel_status,
     input [3:0]     x_status,
     input [3:0]     y_status,
-    
-    output [2:0]    intr
+
+    output [7:0]    out,
+    output [2:1]    intr
 );
 
-    assign wheel_high = 1'b1;
+    localparam X_STATUS_REG     = 8'hA0;
+    localparam Y_STATUS_REG     = 8'hB0;
 
-    parameter WHEEL_STATUS_REG  = 8'h90;
-    parameter X_STATUS_REG      = 8'hA0;
-    parameter Y_STATUS_REG      = 8'hB0;
+    wire            x_signal;
+    reg             x_clr;
+    wire [3:0]      x_sync;
+    wire [3:0]      x_db;
+    sync x0[3:0]    (clk, rst, x_status, x_sync);
+    db   x1[3:0]    (clk, rst, x_sync, x_db);
+    signal_interrupt #(.MSB(4)) x2 (clk, rst, x_clr, 1'b1, x_db, x_signal, intr[1]);
 
-    reg         spi_tx_valid;
-    reg  [7:0]  spi_tx_byte;
-    wire [7:0]  spi_rx_byte;
-    wire        spi_tx_ready;
-    wire        spi_rx_valid;
+    wire            y_signal;
+    reg             y_clr;
+    wire [3:0]      y_sync;
+    wire [3:0]      y_db;
+    sync y0[3:0]    (clk, rst, y_status, y_sync);
+    db   y1[3:0]    (clk, rst, y_sync, y_db);
+    signal_interrupt #(.MSB(4)) y2 (clk, rst, y_clr, 1'b1, y_db, y_signal, intr[2]);
 
-    reg         spi_miso_reg;
+    reg [7:0]   data_addr;
+    reg [7:0]   data_rd_addr;
+    reg         data_read;
+    reg [7:0]   data_in;
+    reg         data_wr_valid;
+    wire [7:0]  data_out;
+    wire        data_rd_valid;
+
+    data_register spi_reg (clk, rst, data_read, data_addr, data_in, data_wr_valid, data_out, data_rd_valid);
+
+    wire spi_clk_sync;
+    wire spi_clk_rise;
+    wire spi_clk_fall;
+    wire spi_clk_db;
+    sync #(.PULL(1'b0)) clk_sync    (clk, rst, spi_clk, spi_clk_sync);
+    db                  clk_db      (clk, rst, spi_clk_sync, spi_clk_db);
+
+    wire spi_cs_sync;
+    wire spi_cs_db;
+    wire spi_cs_rise;
+    wire spi_cs_fall;
+    sync #(.PULL(1'b1)) cs_sync     (clk, rst, spi_cs, spi_cs_sync);
+    db                  cs_db       (clk, rst, spi_cs_sync, spi_cs_db);
+    edges               cs_edges    (clk, rst, spi_cs_db, spi_cs_rise, spi_cs_fall);
+
+    wire spi_mosi_sync;
+    sync #(.PULL(1'b0)) mosi_sync   (clk, rst, spi_mosi, spi_mosi_sync);
+
+    wire        rx_valid;
+    wire        tx_ready;
+    reg         tx_valid;
+    reg  [7:0]  tx_byte;
+    wire [7:0]  rx_byte;
+    reg  [7:0]  spi_byte_num;
     wire        spi_miso_wire;
-    assign      spi_miso = spi_miso_reg;
+    reg         spi_miso_reg;
 
-    spi spi0 
+    spi spi0
     (
         clk,
         rst,
 
-        spi_clk,
-        spi_cs,
-        spi_mosi,
+        spi_clk_db,
+        spi_cs_db,
+        spi_mosi_sync,
         spi_miso_wire,
 
-        spi_tx_byte,
-        spi_rx_byte,
+        tx_byte,
+        rx_byte,
 
-        spi_tx_valid,
-        spi_tx_ready,
-        spi_rx_valid,
+        tx_valid,
+        tx_ready,
+        rx_valid
     );
 
-    wire spi_cs_rise;
-    wire spi_cs_fall;
-    wire spi_cs_out;
-    debounce #(.TICK(10), .MSB(4)) cs_db
-    (clk, rst, 1'b1, spi_cs, 1'b0, spi_cs_rise, spi_cs_fall, spi_cs_out);
-
-    wire [7:0] w_rise;
-    wire [7:0] w_fall;
-    wire [7:0] w_out;
-    debounce #(.TICK(10)) wheel_db[7:0]
-    (clk, rst, 1'b1, {6'h3F, wheel_status}, 1'b1, w_rise, w_fall, w_out);
-
-    wire [7:0] x_rise;
-    wire [7:0] x_fall;
-    wire [7:0] x_out;
-    debounce #(.TICK(10)) x_db[7:0]
-    (clk, rst, 1'b1, {4'hF, x_status}, 1'b1, x_rise, x_fall, x_out);
-
-    wire [7:0] y_rise;
-    wire [7:0] y_fall;
-    wire [7:0] y_out;
-    debounce #(.TICK(10)) y_db[7:0]
-    (clk, rst, 1'b1, {4'hF, y_status}, 1'b1, y_rise, y_fall, y_out);
-
-    reg  w_clr;
-    reg  w_en;
-    wire w_int_new;
-    signal_interrupt    w_sig   (clk, rst, w_clr, w_en, w_out, w_int_new, intr[0]);
-    
-    reg x_clr;
-    reg x_en;
-    wire x_int_new;
-    signal_interrupt    x_sig   (clk, rst, x_clr, x_en, x_out, x_int_new, intr[1]);
-
-    reg y_clr;
-    reg y_en;
-    wire y_int_new;
-    signal_interrupt    y_sig   (clk, rst, y_clr, y_en, y_out, y_int_new, intr[2]);
-
-    reg         data_rw;
-    reg [7:0]   data_addr;
-    reg [7:0]   rd_addr;
-    reg [7:0]   wr_byte;
-    reg         wr_valid;
-    wire [7:0]  rd_byte;
-    wire        rd_valid;
-    data_register   spi_data_bank   (clk, rst, data_rw, data_addr, wr_byte, wr_valid, rd_byte, rd_valid);
-
-    reg x_int_reg;
-    reg x_int_fall;
-    reg y_int_reg;
-    reg y_int_fall;
+    assign out[0]   = x_db[0];
+    assign out[1]   = x_db[1];
+    assign out[2]   = x_db[2];
+    assign out[3]   = x_db[3];
+    assign spi_miso = spi_miso_reg;
 
     initial begin
-        x_int_reg <= 1'b1;
-        y_int_reg <= 1'b1;
-    end
-
-    always @(posedge clk) begin
-        x_int_reg   <= intr[1];
-        x_int_fall  <= ~intr[1] & (intr[1] ^ x_int_reg);
-        y_int_reg   <= intr[2];
-        y_int_fall  <= ~intr[2] & (intr[2] ^ y_int_reg);
-    end
-
-    reg [7:0] spi_byte_num;
-
-    initial begin
-        spi_tx_byte     <= 8'h00;
+        tx_byte         <= 8'h00;
+        tx_valid        <= 1'b0;
         spi_byte_num    <= 8'h00;
-        w_en            <= 1'b1;
-        x_en            <= 1'b1;
-        y_en            <= 1'b1;
     end
 
-
-    // SPI MISO
-    // High impedance (disconnected) when spi inactive (cs high)
     always @(posedge clk) begin
-        if(spi_cs)
+        if(spi_cs_db) begin
             spi_miso_reg    <= 1'bz;
-        else
+        end
+        else begin
             spi_miso_reg    <= spi_miso_wire;
+        end
     end
 
     always @(posedge clk) begin
-        data_rw         <= 1'b0;
-        spi_tx_valid    <= 1'b0;
-        wr_valid        <= 1'b0;
-        w_clr           <= 1'b1;
+        tx_valid        <= 1'b0;
+        data_read       <= 1'b0;
+        data_wr_valid   <= 1'b0;
         x_clr           <= 1'b1;
         y_clr           <= 1'b1;
 
         if(spi_cs_rise) begin
             spi_byte_num    <= 8'h00;
-            case (rd_addr)
-                X_STATUS_REG        : x_clr <= 1'b0;
-                Y_STATUS_REG        : y_clr <= 1'b0;
-                WHEEL_STATUS_REG    : w_clr <= 1'b0;
+            case (data_rd_addr)
+                X_STATUS_REG    : x_clr <= 1'b0;
+                Y_STATUS_REG    : y_clr <= 1'b0;
             endcase
         end
 
-        if(spi_rx_valid) begin
+        if(rx_valid) begin
             if(spi_byte_num == 8'h00) begin
-                rd_addr     <= spi_rx_byte;
-                data_addr   <= spi_rx_byte;
-                data_rw     <= 1'b1;
+                data_rd_addr    <= rx_byte;
+                data_addr       <= rx_byte;
+                data_read       <= 1'b1;
             end
-            spi_byte_num    <= spi_byte_num + 1'b1;
+            spi_byte_num        <= spi_byte_num + 1'b1;
         end
 
-        if(rd_valid) begin
-            spi_tx_byte     <= rd_byte;
+        if(data_rd_valid) begin
+            tx_byte         <= data_out;
         end
 
-        if(spi_tx_ready) begin
-            spi_tx_valid    <= 1'b1;
+        if(tx_ready) begin
+            tx_valid        <= 1'b1;
         end
 
-        if(x_int_new) begin
+        if(x_signal) begin
             data_addr       <= X_STATUS_REG;
-            wr_byte         <= x_out;
-            wr_valid        <= 1'b1;
+            data_wr_valid   <= 1'b1;
+            data_in         <= {4'hF, x_db};
         end
-        else if(y_int_new) begin
+        else if(y_signal) begin
             data_addr       <= Y_STATUS_REG;
-            wr_byte         <= y_out;
-            wr_valid        <= 1'b1;
-        end
-        else if(w_int_new) begin
-            data_addr       <= WHEEL_STATUS_REG;
-            wr_byte         <= w_out;
-            wr_valid        <= 1'b1;
+            data_wr_valid   <= 1'b1;
+            data_in         <= {4'hF, y_db};
         end
     end
 
