39c906058304a2b772faa13406585240e73e4cba
cavityQED <mikeped@udel.edu>
2024-07-18 14:15:08 -0400

    pincPosition Update
    Removes use of LCD number
    Uses new group box palette

--- gui/pincPosition.cpp
+++ gui/pincPosition.cpp
@@ -20,21 +20,27 @@ pincPosition::pincPosition(PINC_AXIS axis, QWidget* parent) : QWidget(parent)
 			break;
 	}
 
-	QLabel*	label = new QLabel(str);
-	label->setGeometry(0,0,20,40);
-	
-	m_lcd = new QLCDNumber();
-	m_lcd->setGeometry(40,0,100,40);
+	QFont linefont;
+	linefont.setWeight(QFont::Bold);
+	m_line = new QLineEdit();
+	m_line->setFont(linefont);
+	m_line->setPalette(pincStyle::pincLineEditPalette);
+	m_line->setAlignment(Qt::AlignRight);
+	m_line->setMinimumHeight(50);
+	m_line->setReadOnly(true);
 
-	QHBoxLayout* layout	= new QHBoxLayout();
-	layout->setSizeConstraint(QLayout::SetFixedSize);
+	QLabel*			label	= new QLabel(str);
+	QHBoxLayout*	layout	= new QHBoxLayout();
+
+	label->setPalette(pincStyle::pincGroupBoxPalette);
 	layout->addWidget(label);
-	layout->addWidget(m_lcd);
+	layout->addWidget(m_line);
 
 	setLayout(layout);
+	set(0);
 }
 
 void pincPosition::set(double pos)
 {
-	m_lcd->display(pos);
+	m_line->setText(QString::number(pos, 'f', 2));
 }
--- gui/pincPosition.h
+++ gui/pincPosition.h
@@ -2,11 +2,19 @@
 #define PINC_POSITION_H
 
 #include <QWidget>
+#include <QFrame>
+#include <QLineEdit>
 #include <QLabel>
 #include <QLCDNumber>
 #include <QHBoxLayout>
+#include <QDebug>
 
 #include "c/stepper/stepper_common.h"
+#include "pincPanel.h"
+#include "pincColors.h"
+#include "pincLineEdit.h"
+#include "pincLabel.h"
+#include "pincStyle.h"
 
 class pincPosition : public QWidget
 {
@@ -17,14 +25,16 @@ public:
 	pincPosition(PINC_AXIS axis, QWidget* parent = nullptr);
 	~pincPosition() {}
 
+	const QLineEdit* line() {return m_line;}
+
 public:
 
 	void set(double pos);
 
 protected:
 
-	QLCDNumber*	m_lcd;
-
+	QLineEdit*	m_line;
+	
 };
 
 #endif
