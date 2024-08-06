b304f5aa2eef22d7eae4b8d6fe1778de52d11212
cavityQED <mikeped@udel.edu>
2024-08-06 14:38:02 -0400

    includes

--- gui/pinc_gui.h
+++ gui/pinc_gui.h
@@ -2,9 +2,16 @@
 #define PINC_GUI
 
 #include "gui/pincMainWindow.h"
+#include "gui/pincCentralWindow.h"
 #include "gui/pincControlModeButtons.h"
 #include "gui/pincJogControl.h"
 #include "gui/pincStepperControl.h"
 #include "gui/pincStepperMove.h"
+#include "gui/pincPanel.h"
+#include "gui/pincStyle.h"
+#include "gui/pincButton.h"
+#include "gui/pincLineEdit.h"
+#include "gui/pincEditWindow.h"
+#include "gui/pincLabel.h"
 
 #endif
