8cb06d8ecf6ed9a509ce6cfaa30f6654838ea3f5
cavityQED <mikeped@udel.edu>
2024-08-06 14:17:28 -0400

    Revert "Removes old gui test files"
    This reverts commit aaaec5886e22aa37f9e304164ad7a94bc46f66c8.

---
+++ gui/pinc_gui.h
@@ -0,0 +1,10 @@
+#ifndef PINC_GUI
+#define PINC_GUI
+
+#include "gui/pincMainWindow.h"
+#include "gui/pincControlModeButtons.h"
+#include "gui/pincJogControl.h"
+#include "gui/pincStepperControl.h"
+#include "gui/pincStepperMove.h"
+
+#endif
